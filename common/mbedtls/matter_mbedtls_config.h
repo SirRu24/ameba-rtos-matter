143ddf1067afd685dc9961b2e4d711cbeee5432c
SirRu24 <mna.bro6@gmail.com>
2024-10-21 10:29:14 +0800

    [sdk] ameba-rtos support
    * AmebaLite support
    * Updated matter_wifi module

--- common/mbedtls/matter_mbedtls_config.h
+++ common/mbedtls/matter_mbedtls_config.h
@@ -29,7 +29,7 @@
 #ifndef MBEDTLS_CONFIG_H
 #define MBEDTLS_CONFIG_H
 
-#if defined(CONFIG_PLATFORM_AMEBADPLUS) || defined(CONFIG_PLATFORM_AMEBASMART)
+#if defined(CONFIG_PLATFORM_AMEBADPLUS) || defined(CONFIG_PLATFORM_AMEBASMART) || defined(CONFIG_PLATFORM_AMEBALITE)
 #include <platform_autoconf.h>
 #endif
 
@@ -42,7 +42,7 @@
 #include <rom_ssl_ram_map.h>
 #define RTL_HW_CRYPTO
 #define SUPPORT_HW_SW_CRYPTO
-#elif defined(CONFIG_PLATFORM_AMEBADPLUS) || defined(CONFIG_PLATFORM_AMEBASMART)
+#elif defined(CONFIG_PLATFORM_AMEBADPLUS) || defined(CONFIG_PLATFORM_AMEBASMART) || defined(CONFIG_PLATFORM_AMEBALITE)
 #include <rom_ssl_ram_map.h>
 #define MBEDTLS_VERSION_CONVERT(a,b,c)	(((a) << 16) + ((b) << 8) + (c))
 #ifndef MBEDTLS_VERSION
@@ -3538,7 +3538,7 @@
 #if (MBEDTLS_VERSION_NUMBER < 0x03000000)
 #if defined(CONFIG_PLATFORM_8710C) || defined(CONFIG_PLATFORM_8721D)
 #include <check_config.h>
-#elif defined(CONFIG_PLATFORM_AMEBADPLUS) || defined(CONFIG_PLATFORM_AMEBASMART)
+#elif defined(CONFIG_PLATFORM_AMEBADPLUS) || defined(CONFIG_PLATFORM_AMEBASMART) || defined(CONFIG_PLATFORM_AMEBALITE)
 #include <mbedtls/check_config.h>
 #endif
 #endif
